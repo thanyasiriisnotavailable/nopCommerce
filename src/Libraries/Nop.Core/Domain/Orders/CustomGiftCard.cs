e5ac2775ca042a98ef5e61332b0098a2434c766d
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-02 11:25:48 +0700

    provided entity

---
+++ src/Libraries/Nop.Core/Domain/Orders/CustomGiftCard.cs
@@ -0,0 +1,15 @@
+﻿namespace Nop.Core.Domain.Orders;
+public partial class CustomGiftCard : BaseEntity
+{
+    public int OrderId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public string RecipientName { get; set; }
+
+    public string Message { get; set; }
+
+    public string Style { get; set; }
+
+    public DateTime CreatedOnUtc { get; set; }
+}
---
+++ src/Libraries/Nop.Data/Mapping/Builders/Orders/CustomGiftCardBuilder.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Customers;
+using Nop.Data.Extensions;
+
+namespace Nop.Data.Mapping.Builders.Orders;
+public partial class CustomGiftCardBuilder : NopEntityBuilder<CustomGiftCard>
+{
+    #region Methods
+
+    /// <summary>
+    /// Apply entity configuration
+    /// </summary>
+    /// <param name="table">Create table expression builder</param>
+    public override void MapEntity(CreateTableExpressionBuilder table)
+    {
+        table
+            .WithColumn(nameof(CustomGiftCard.OrderId)).AsInt32().ForeignKey<Order>().NotNullable()
+            .WithColumn(nameof(CustomGiftCard.CustomerId)).AsInt32().ForeignKey<Customer>().NotNullable()
+            .WithColumn(nameof(CustomGiftCard.RecipientName)).AsString(200).Nullable()
+            .WithColumn(nameof(CustomGiftCard.Message)).AsString(int.MaxValue).Nullable()
+            .WithColumn(nameof(CustomGiftCard.Style)).AsString(100).NotNullable()
+            .WithColumn(nameof(CustomGiftCard.CreatedOnUtc)).AsDateTime().NotNullable();
+    }
+
+    #endregion
+}
