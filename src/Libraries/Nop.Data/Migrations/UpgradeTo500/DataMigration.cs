99128d384d2652cb237ce4c489d6d1b1961b44c0
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-03 00:08:08 +0700

    added migration

---
+++ src/Libraries/Nop.Data/Migrations/UpgradeTo500/DataMigration.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.Orders;
+using Nop.Data.Extensions;
+
+namespace Nop.Data.Migrations.UpgradeTo500;
+[NopSchemaMigration("2026-02-02 11:00:00", "Create CustomGiftCard table")]
+public class CreateCustomGiftCardTable : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.TableFor<CustomGiftCard>();
+    }
+}
