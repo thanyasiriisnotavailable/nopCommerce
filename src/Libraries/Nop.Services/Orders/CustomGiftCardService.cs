6983e0ccb095b6ebb93e46edf10e2d3420a645b4
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2026-02-03 10:20:45 +0700

    added service

---
+++ src/Libraries/Nop.Services/Orders/CustomGiftCardService.cs
@@ -0,0 +1,25 @@
+﻿using Nop.Core.Domain.Orders;
+using Nop.Data;
+
+namespace Nop.Services.Orders;
+public class CustomGiftCardService : ICustomGiftCardService
+{
+    private readonly IRepository<CustomGiftCard> _giftCardRepository;
+
+    public CustomGiftCardService(IRepository<CustomGiftCard> giftCardRepository)
+    {
+        _giftCardRepository = giftCardRepository;
+    }
+
+    public async Task InsertAsync(CustomGiftCard giftCard)
+    {
+        await _giftCardRepository.InsertAsync(giftCard);
+    }
+
+    public async Task<IList<CustomGiftCard>> GetByOrderIdAsync(int orderId)
+    {
+        return await _giftCardRepository.Table
+            .Where(x => x.OrderId == orderId)
+            .ToListAsync();
+    }
+}
---
+++ src/Libraries/Nop.Services/Orders/ICustomGiftCardService.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Core.Domain.Orders;
+
+namespace Nop.Services.Orders;
+public interface ICustomGiftCardService
+{
+    Task InsertAsync(CustomGiftCard giftCard);
+    Task<IList<CustomGiftCard>> GetByOrderIdAsync(int orderId);
+}
+
